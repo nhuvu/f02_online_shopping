827099cd9a7c000de9eb835f7224c90dc8cb0fb1
Arale <nhu.vu48@gmail.com>
2025-05-21 21:55:17 +0700

    l23 orm

--- src/main/java/com/example/f02_online_shopping/repository/CartRepository.java
+++ src/main/java/com/example/f02_online_shopping/repository/CartRepository.java
@@ -1,4 +1,9 @@
 package com.example.f02_online_shopping.repository;
 
-public interface CartRepository {
+import com.example.f02_online_shopping.entity.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart, Integer> {
 }
