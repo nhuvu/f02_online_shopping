e4267c475946a4c89606d3725537ddaee25836cc
nhu.vu <nhu.vu@lazada.com>
2025-06-10 18:03:31 +0700

    l27-microservices: break services

---
+++ shared-libs/src/main/java/com/example/sharedlibs/SharedLibsApplication.java
@@ -0,0 +1,13 @@
+package com.example.sharedlibs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedLibsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharedLibsApplication.class, args);
+    }
+
+}
